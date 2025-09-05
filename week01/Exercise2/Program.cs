29cbe65d48e36120c8a625d7ea38c7a073d7c089
arcarver <acarver@byupathway.edu>
2025-09-05 16:29:06 -0700

    part 1 done

--- week01/Exercise2/Program.cs
+++ week01/Exercise2/Program.cs
@@ -1,9 +1,33 @@
 using System;
+using System.Linq.Expressions;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise2 Project.");
+        Console.Write("What is your grade percentage? ");
+        string gradePercentage = Console.ReadLine();
+        int grade = int.Parse(gradePercentage);
+
+        if (grade >= 90)
+        {
+            Console.WriteLine("Your grade is an A");
+        }
+        else (grade < 90 && grade >= 80)
+        {
+            Console.WriteLine("Your grade is an B");
+        }
+        else (grade < 80 && grade >= 70)
+        {
+            Console.WriteLine("Your grade is an C");
+        }
+        else (grade < 70 && grade >= 60)
+        {
+            Console.WriteLine("Your grade is an D");
+        }
+        else (grade < 60)
+        {
+            Console.WriteLine("Your grade is an F");
+        }
     }
 }
