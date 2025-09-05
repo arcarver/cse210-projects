83e93db90f0d3de195996a9e8e10eebc999bcbf3
arcarver <acarver@byupathway.edu>
2025-09-05 15:36:39 -0700

    bond names

--- week01/Exercise1/Program.cs
+++ week01/Exercise1/Program.cs
@@ -4,7 +4,13 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("What is your first name?");
+        Console.Write("What is your first name? ");
         string first = Console.ReadLine();
+
+        Console.Write("What is your last name? ");
+        string last = Console.ReadLine();
+
+        Console.WriteLine("");
+        Console.WriteLine($"Your name is {last}, {first} {last}.");
     }
 }
