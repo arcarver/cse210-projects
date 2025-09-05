8ef4320fa2269f82b3b02d88f8fc4cbb25c10b15
arcarver <acarver@byupathway.edu>
2025-09-05 15:30:03 -0700

    first

--- week01/Exercise1/Program.cs
+++ week01/Exercise1/Program.cs
@@ -4,6 +4,7 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise1 Project.");
+        Console.WriteLine("What is your first name?");
+        string first = Console.WriteLine();
     }
 }
