d9c551ac9c1afde3cbc3dc96dbb54b5118cc1cd0
arcarver <acarver@byupathway.edu>
2025-09-05 20:30:27 -0700

    part 1 and 2

--- week01/Exercise4/Program.cs
+++ week01/Exercise4/Program.cs
@@ -1,9 +1,41 @@
 using System;
+using System.Diagnostics.CodeAnalysis;
+using System.Transactions;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise4 Project.");
+        Console.WriteLine("Enter a list of numbers. Type 0 when finished.");
+        List<int> numbers;
+        numbers = new List<int>();
+        int number = 45;
+        do
+        {
+            Console.Write("Enter number: ");
+            string numberString = Console.ReadLine();
+            number = int.Parse(numberString);
+            if (number != 0)
+            {
+                numbers.Add(number);
+            }
+        } while (number != 0);
+
+        int total = numbers.Sum();
+        Console.WriteLine($"The sum is: {total}");
+        float totalItems = numbers.Count;
+        float average = total / totalItems;
+        Console.WriteLine($"The average is: {average}");
+
+
+
+
+        // foreach (int item in numbers)
+        // {
+        //     Console.WriteLine(item);
+        // }
+
     }
 }
