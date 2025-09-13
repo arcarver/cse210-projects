1cf6d8693984ee1d03a9b9fa60a79c948e0f97dd
arcarver <acarver@byupathway.edu>
2025-09-13 11:25:58 -0700

    finally works

--- week02/Resumes/Resume.cs
+++ week02/Resumes/Resume.cs
@@ -4,7 +4,16 @@ using System.Threading.Tasks.Dataflow;
 public class Resume
 {
     public string _name;
-    public List<Job> _jobs;
-    List<string> Job = new List<string>();
-    
+    public List<Job> _jobs = new List<Job>();
+    List<Job> _jobs = new List<Job>();
+    public void Display()
+    {
+        Console.WriteLine($"Name: {_name}");
+        Console.WriteLine("Jobs:");
+
+        foreach (Job job in _jobs)
+        {
+            job.Display();
+        }
+    }
 }
