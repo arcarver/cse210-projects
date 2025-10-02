6e185df62c2a54dff347d2921af990c51a50505d
arcarver <acarver@byupathway.edu>
2025-10-01 22:48:14 -0700

    getsummary

--- week05/Homework/Program.cs
+++ week05/Homework/Program.cs
@@ -5,5 +5,8 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello World! This is the Homework Project.");
+        Assignment name1 = new Assignment("Suzie", "science");
+
+        Console.WriteLine(name1.GetSummary());
     }
 }
