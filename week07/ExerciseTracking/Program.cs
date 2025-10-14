b6aefc6a9efc90655c0cba5501286b557eac5161
arcarver <acarver@byupathway.edu>
2025-10-14 11:18:31 -0700

    change variables

--- week07/ExerciseTracking/Program.cs
+++ week07/ExerciseTracking/Program.cs
@@ -8,9 +8,9 @@ class Program
         Console.WriteLine("Hello World! This is the ExerciseTracking Project.");
         Running run1 = new Running("Nov 2, 2025", 60, 15);
         Console.WriteLine(run1.GetSummary());
-        Swimming swim1 = new Swimming("May 7, 2025", 50, 19);
+        Swimming swim1 = new Swimming("May 7, 2025", 15, 20);
         Console.WriteLine(swim1.GetSummary());
-        Cycling cycle1 = new Cycling("June 4, 1977", 45, 32);              
+        Cycling cycle1 = new Cycling("June 4, 1977", 45, 3);              
         Console.WriteLine(cycle1.GetSummary());
     }
 }
